7094646c2c52615c1784b0d9673aed50ebb9d3a1
Mathieu Poupon <90232345+mpoupon@users.noreply.github.com>
2026-01-24 21:36:03 -0500

    Delete dashboard-oae directory

--- dashboard-oae/shaders/vertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vertexUV;
-varying vec3 vertexNormal;
-
-void main() {
-    vertexUV = uv; // Pass the UV coordinates to the fragment shader
-    vertexNormal = normalize(normalMatrix * normal); // Pass the normal vector to the fragment shader
-    
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); // Set the position of the vertex
-}
